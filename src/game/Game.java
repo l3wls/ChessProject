e032ce213789ed2b6336186bf3291ba9ec80c985
lewis <lewisjuliofigueroa23@yahoo.com>
2025-10-08 14:40:22 -0500

    created game class with constructor,instance variables and void start class

--- src/game/Game.java
+++ src/game/Game.java
@@ -2,3 +2,23 @@ package game;
 
 import board.Board;
 import player.Player;
+public class Game {
+    private Board board;
+    private Player whitePlayer;
+    private Player blackPlayer;
+    private String currentTurn;
+
+    public Game() {
+        board = new Board();
+        whitePlayer = new Player("white");
+        blackPlayer = new Player("black");
+        currentTurn = "white";
+    }
+
+    public void start() {
+        System.out.println("Welcome to Chess!");
+        System.out.println("Enter moves in the format: [FROM] [TO] (e.g., E2 E4)");
+        System.out.println();
+
+    }
+}
