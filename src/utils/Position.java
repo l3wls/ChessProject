fdfe416aad44e19b8da56296247102bcabf85deb
lewis <lewisjuliofigueroa23@yahoo.com>
2025-10-08 13:15:09 -0500

    constructs a Position object from chess notation and Position object with specified row and column

--- src/utils/Position.java
+++ src/utils/Position.java
@@ -1,4 +1,26 @@
 package utils;
 public class Position {
+    private int row;
+    private int col;
 
+    public Position(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public Position(String chessNotation) {
+        if (chessNotation.length() != 2) {
+            throw new IllegalArgumentException("Invalid chess notation: " + chessNotation);
+        }
+
+        char colChar = Character.toUpperCase(chessNotation.charAt(0));
+        char rowChar = chessNotation.charAt(1);
+
+        if (colChar < 'A' || colChar > 'H' || rowChar < '1' || rowChar > '8') {
+            throw new IllegalArgumentException("Invalid chess notation: " + chessNotation);
+        }
+
+        this.col = colChar - 'A';
+        this.row = '8' - rowChar;
+    }
 }
