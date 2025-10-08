bad762b4b80f0eee22cfe4200dcf9ee9971edd65
lewis <lewisjuliofigueroa23@yahoo.com>
2025-10-08 13:39:26 -0500

    Created Piece class with constructor and getter methods

--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -2,3 +2,22 @@ package pieces;
 
 import utils.Position;
 import java.util.List;
+
+public abstract class Piece {
+    protected String color;
+    protected Position position;
+    protected String symbol;
+
+    public Piece(String color, Position position) {
+        this.color = color;
+        this.position = position;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+}
