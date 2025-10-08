3b22a7d435e9a9863f0af5cb03f1433ba9f4e1d7
lewis <lewisjuliofigueroa23@yahoo.com>
2025-10-08 14:26:18 -0500

    added constructor of Player object with the specified color

--- src/player/Player.java
+++ src/player/Player.java
@@ -4,3 +4,14 @@ import board.Board;
 import utils.Position;
 import java.util.Scanner;
 
+public class Player {
+    private String color;
+
+    public Player(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
