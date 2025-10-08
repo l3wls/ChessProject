2e884d5891e46058d9a953e7cce57b94a1079b97
lewis <lewisjuliofigueroa23@yahoo.com>
2025-10-08 13:42:12 -0500

    added to show list possible moves, isvalidMove, and move method

--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -20,4 +20,27 @@ public abstract class Piece {
     public Position getPosition() {
         return position;
     }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public abstract List<Position> possibleMoves(Piece[][] board);
+
+    public boolean isValidMove(Piece[][] board, Position target) {
+        return possibleMoves(board).contains(target);
+    }
+
+    public void move(Position newPosition) {
+        this.position = newPosition;
+    }
+
+    @Override
+    public String toString() {
+        return symbol;
+    }
 }
